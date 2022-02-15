3e2c6da0c967c24d833c832662ebb1978a7b9ab7
= <sartakov.sasha@icloud.com>
2022-02-15 21:35:02 +0700

    Feeling stupid with tests

--- Python/handSchool.py
+++ Python/handSchool.py
@@ -4,10 +4,11 @@ import random
 At the input, the function get natural number
 You need to generate n arrays of unique size, fill them with random numbers
 Then the arrays needed to be sorted:
-    Arrays with an even oridinal number should be sorted in ascending order
+    Arrays with an even ordinal number should be sorted in ascending order
     Other - in descending order
 Function returns the array of arrays
 """
+
 
 def q_sort(a: list):
     if a:
@@ -18,63 +19,67 @@ def q_sort(a: list):
     else:
         return a
 
-def sorted_arrays_array(n: int):
 
+def sorted_arrays_array(n: int):
     if n < 1:
         raise TypeError('Not natural number')
 
-    main_array = [] # an array of arrays
-    len_array = [] # set of len of arrays
+    main_array = []  # an array of arrays
+    len_array = []  # set of len of arrays
     for i in range(n):
-        num = random.randint(1, n) # choose a random number
-        while num in len_array: # if the len is already in the array change it
-            num = random.randint(1, n * 2) # enlarge the bounds of picking
-        len_array.append(num) # I don't think this should be used
+        num = random.randint(1, n)  # choose a random number
+        while num in len_array:  # if the len is already in the array change it
+            num = random.randint(1, n * 2)  # enlarge the bounds of picking
+        len_array.append(num)  # I don't think this should be used
     for i in range(n):
-        tmp = [round(random.random() * 1000) for j in range(len_array[i])] # fill the array with random numbers
+        tmp = [round(random.random() * 1000) for _ in range(len_array[i])]  # fill the array with random numbers
         main_array.append(tmp)
 
-    for i in range(n): # sort the arrays by the rules
+    for i in range(n):  # sort the arrays by the rules
         if i % 2 == 0:
             main_array[i] = q_sort(main_array[i])
         else:
             main_array[i] = q_sort(main_array[i])[::-1]
     return main_array
 
-def test_func(result: list) -> bool:
+
+def test_array_size(result: list):
     test = []
     for i in result:
         if len(i) in test:
+            print(result)
             return False
         else:
             test.append(len(i))
     return True
 
-def test_sorted(result : list) -> bool:
-    for i in result:
+
+def test_sorted(result: list):
+    for i in range(len(result)):
         if i % 2 == 0:
             tmp = result[i][:]
             tmp.sort()
             if tmp != result[i]:
+                print(i)
                 return False
         else:
             tmp = result[i][::-1]
             tmp.sort()
             if tmp != result[i][::-1]:
+                print(i)
                 return False
     return True
-
 
 
 def main():
     result = sorted_arrays_array(int(input()))
 
-    if not test_func(result) or test_sorted(result):
-        print("You failed this task")
+    if test_array_size(result) and test_sorted(result):
+        print("The task is done")
         return
 
-    for i in result:
-        print(F"len of array is {len(i)}\n{i}")
+    print("You failed this task")
+
 
 if __name__ == '__main__':
     main()
