eab4f5c580a12b2090f1a937be1d6e6e339bae1e
= <sartakov.sasha@icloud.com>
2022-02-20 21:51:10 +0700

    New task

--- Heads&Hands/Go/main.go
+++ Heads&Hands/Go/main.go
@@ -1,9 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
 
 func main() {
 	var n int
-	fmt.Scan(&n)
-	
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		return
+	}
+	array := testFunc(n)
+	for _, i := range array {
+		fmt.Println(len(i), i)
+	}
+}
+
+func testFunc(n int) [][]int {
+	if n < 1 {
+		return nil
+	}
+	var mainArray [][]int
+	lenArray := func(arr []int) []int {
+		for i := 0; i < n; i++ {
+			arr[i] += 1
+		}
+		return arr
+	}(rand.Perm(n))
+
+	for i := 0; i < n; i++ {
+		tmpArray := func() (result []int) {
+			for j := 0; j < lenArray[i]; j++ {
+				result = append(result, rand.Int()%1000)
+			}
+			return
+		}()
+		mainArray = append(mainArray, tmpArray)
+	}
+	for i := 0; i < n; i++ {
+		sort.Ints(mainArray[i])
+		if i%2 != 0 {
+			for j := 0; j < len(mainArray[i])/2; j++ {
+				mainArray[i][j], mainArray[i][len(mainArray[i])-j-1] = mainArray[i][len(mainArray[i])-j-1], mainArray[i][j]
+			}
+		}
+	}
+	return mainArray
 }
